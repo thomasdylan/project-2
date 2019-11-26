4ce4c9a349a0aada6d4827832d2e27ccdfe75d58
Randamus <rcviets@gmail.com>
2019-11-25 21:22:07 -0700

    apiRoute names changed/avatar.js file for sign up page use/newPost boilerplate

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,21 +2,21 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
+  app.get("/api/reviews", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
+  app.post("/api/reviews", function(req, res) {
     db.Example.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
+  app.delete("/api/reviews/:id", function(req, res) {
     // eslint-disable-next-line prettier/prettier
     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
       res.json(dbExample);
