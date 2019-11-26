70ae7aeda828b146bbec8c43f27af566fb6b3d7e
Randamus <rcviets@gmail.com>
2019-11-25 20:51:56 -0700

    cartoon avatar selections/buttons for sign up page

---
+++ public/js/avatar.js
@@ -0,0 +1,22 @@
+var avatar = require("cartoon-avatar");
+var url;
+
+$(document).on("click", "#male", function() {
+  url = avatar.generate_avatar({ "gender": "male" });
+  $("#avi").attr("src", url);
+});
+
+$(document).on("click", "#female", function() {
+  url = avatar.generate_avatar({ "gender": "female" });
+  $("#avi").attr("src", url);
+});
+
+$(document).on("click", "#unspecified", function() {
+  $("#avi").attr("src", "/images/unspecifiedAvatar");
+});
+
+//Buttons for Sign Up Page
+
+/* <button type="button" class="btn btn-primary" id="male">Male</button>
+<button type="button" class="btn btn-primary" id="female">Female</button>
+<button type="button" class="btn btn-primary" id="unspecified">Unspecified</button> */
