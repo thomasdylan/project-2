73955e837761cd6dc5d2d76156a0bdfdb834f69c
Randamus <rcviets@gmail.com>
2019-11-25 20:55:25 -0700

    formatting

--- public/js/avatar.js
+++ public/js/avatar.js
@@ -1,6 +1,13 @@
 var avatar = require("cartoon-avatar");
 var url;
 
+//Buttons for Sign Up Page
+
+/* <button type="button" class="btn btn-primary" id="male">Male</button>
+<button type="button" class="btn btn-primary" id="female">Female</button>
+<button type="button" class="btn btn-primary" id="unspecified">Unspecified</button> */
+
+//On Button Clicks - Generate Random Avatar
 $(document).on("click", "#male", function() {
   url = avatar.generate_avatar({ "gender": "male" });
   $("#avi").attr("src", url);
@@ -14,9 +21,3 @@ $(document).on("click", "#female", function() {
 $(document).on("click", "#unspecified", function() {
   $("#avi").attr("src", "/images/unspecifiedAvatar");
 });
-
-//Buttons for Sign Up Page
-
-/* <button type="button" class="btn btn-primary" id="male">Male</button>
-<button type="button" class="btn btn-primary" id="female">Female</button>
-<button type="button" class="btn btn-primary" id="unspecified">Unspecified</button> */
