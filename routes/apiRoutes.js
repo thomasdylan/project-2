4ce8473a1aff94efe6dffc75c4f6b1eadfe64ae8
Randamus <rcviets@gmail.com>
2019-12-04 19:24:33 -0700

    apiRoutes rename//flushQT file destroy

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,23 +3,23 @@ var db = require("../models");
 module.exports = function(app) {
   // Get All Reviews
   app.get("/api/reviews", function(req, res) {
-    db.Flush.findAll({}).then(function(dbFlush) {
-      res.json(dbFlush);
+    db.Users.findAll({}).then(function(dbUsers) {
+      res.json(dbUsers);
     });
   });
 
   // Create A New Review
   app.post("/api/reviews", function(req, res) {
-    db.Flush.create(req.body).then(function(dbFlush) {
-      res.json(dbFlush);
+    db.Users.create(req.body).then(function(dbUsers) {
+      res.json(dbUsers);
     });
   });
 
   // Delete A Review By Id
   app.delete("/api/reviews/:id", function(req, res) {
     // eslint-disable-next-line prettier/prettier
-    db.Flush.destroy({ where: { id: req.params.id } }).then(function(dbFlush) {
-      res.json(dbFlush);
+    db.Users.destroy({ where: { id: req.params.id } }).then(function(dbUsers) {
+      res.json(dbUsers);
     });
   });
 };
