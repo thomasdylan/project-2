a5c6b79e68248b2933e67125c1393b53287add81
lpettit1 <50005432+lpettit1@users.noreply.github.com>
2019-12-04 21:06:33 -0700

    fix seed to match post

--- db/seed.sql
+++ db/seed.sql
@@ -1,9 +1,9 @@
 
 
-INSERT INTO restroom (location, rateRestroom, cleanliness, tmi) VALUES ("the philadelphian, 2, 2, dark and dirty good just for washing your hands.");
+INSERT INTO restroom (title, tmi) VALUES ("the philadelphian, 2, dark and dirty good just for washing your hands.");
 
-INSERT INTO restroom (location, rateRestroom, cleanliness, tmi) VALUES ("maverik on 11415 s. 700 e., 3, 3, if you need to go it's stop.");
+INSERT INTO restroom (title, rating, tmi) VALUES ("maverik on 11415 s. 700 e., 3, if you need to go it's stop.");
 
-INSERT INTO restroom (location, rateRestroom, cleanliness, tmi) VALUES ("five guys on 487 e. 12300 s., 4, 4, you can sit and feel clean.");
+INSERT INTO restroom (title, rating, tmi) VALUES ("five guys on 487 e. 12300 s., 4, you can sit and feel clean.");
 
-INSERT INTO restroom (location, rateRestroom, cleanliness, tmi) VALUES ("mountain america credit union 12221 s. state st, 5, 5, you can lay on the floor and be clean.");
+INSERT INTO restroom (title, rating, tmi) VALUES ("mountain america credit union 12221 s. state st, 5, you can lay on the floor and be clean.");
