2b5a77a5182b88192470526140d8db156a2116d7
lpettit1 <50005432+lpettit1@users.noreply.github.com>
2019-12-07 09:52:12 -0700

    add a second schema for the user data

--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,5 @@
 
---post.js dataseed
+--use in flushdb dataseed
 INSERT INTO restroom (title,ratimg, tmi) VALUES ("The Philadelphian, 2, dark and dirty good just for washing your hands.");
 INSERT INTO restroom (title, rating, tmi) VALUES ("Maverik on 11415 s. 700 e., 3, if you need to go it's stop.");
 INSERT INTO restroom (title, rating, tmi) VALUES ("Five guys on 487 e. 12300 s., 4, you can sit and feel clean.");
@@ -9,6 +9,6 @@ INSERT INTO restroom (title, rating, tmi) VALUES ("Barnes & Noble 10180 state st
 INSERT INTO restroom (title, rating, tmi) VALUES ("Abravanel Hall 123 w. south temple, 5, very clean and nice.");
 INSERT INTO restroom (title, rating, tmi) VALUES ("Vivint Smart Home Arena, 1, better take your own tolit cover.");
 
---user.js dataseed
+--use in userdb dataseed
 INSERT INTO users (userName, email, password, gender) VALUES ("jane56, jane56@gmail.com, 11111111, F");
 INSERT INTO users (userName, email, password, gender) VALUES ("bob99, bob99@gmail.com, 11111111, M");
