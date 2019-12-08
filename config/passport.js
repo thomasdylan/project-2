7cae1bd894e3adf21fdb8832e8c2e83f6f56a2ac
Dylan Thomas <thomasdylan76@gmail.com>
2019-12-07 20:56:51 -0700

    Users can sign up and login.
    My mental sanity is gone.

---
+++ config/passport.js
@@ -0,0 +1,109 @@
+var bCrypt = require("bcryptjs");
+var db = require("../models");
+
+module.exports = function(passport, user) {
+  var User = user;
+  var LocalStrategy = require("passport-local").Strategy;
+
+  passport.use(
+    "local-signup",
+    new LocalStrategy(
+      {
+        usernameField: "email",
+        passwordField: "password",
+        passReqToCallback: true
+      },
+      function(req, email, password, done) {
+        var generateHash = function(password) {
+          return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
+        };
+
+        db.Users.findOne({
+          where: {
+            email: email
+          }
+        }).then(function(user) {
+          if (user) {
+            return done(null, false, {
+              message: "An account already exist for this email"
+            });
+          } else {
+            var userPassword = generateHash(password);
+            var userData = {
+              userName: req.body.userName,
+              email: email,
+              password: userPassword,
+              gender: req.body.gender
+            };
+            db.Users.create(userData).then(function(newUser, created) {
+              if (!newUser) {
+                return done(null, false);
+              }
+              if (newUser) {
+                return done(null, newUser);
+              }
+            });
+          }
+        });
+      }
+    )
+  );
+
+  //LOCAL SIGNIN
+  passport.use(
+    "local-signin",
+    new LocalStrategy(
+      {
+        usernameField: "email",
+        passwordField: "password",
+        passReqToCallback: true
+      },
+      function(req, email, password, done) {
+        var User = user;
+        var isValidPassword = function(userPassword, password) {
+          return bCrypt.compareSync(password, userPassword);
+        };
+        db.Users.findOne({
+          where: {
+            email: email
+          }
+        })
+          .then(function(user) {
+            if (!user) {
+              return done(null, false, {
+                message: "Email does not exist"
+              });
+            }
+            if (!isValidPassword(user.password, password)) {
+              return done(null, false, {
+                message: "Incorrect password."
+              });
+            }
+            var userInfo = user.get();
+            return done(null, userInfo);
+          })
+          .catch(function(err) {
+            console.log("Error: ", err);
+            return done(null, false, {
+              message: "Something went wrong with your login."
+            });
+          });
+      }
+    )
+  );
+
+  //serialize
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  //deserialize
+  passport.deserializeUser(function(id, done) {
+    db.Users.findOne({ where: { id: id } }).then(function(user) {
+      if (user) {
+        done(null, user.get());
+      } else {
+        done(user.errors, null);
+      }
+    });
+  });
+};
--- routes/userApiRoutes.js
+++ routes/userApiRoutes.js
@@ -1,122 +1,85 @@
-var db = require("../models");
-var bcrypt = require("bcryptjs");
-var jwt = require("jsonwebtoken");
+//var db = require("../models");
+var authControl = require("./authRoutes.js");
 
-process.env.SECRET_KEY = "secret";
+process.env.SECRET_KEY = "yougotflushed";
 
-module.exports = function(app) {
+module.exports = function(app, passport) {
   //REGISTER
-  app.post("/api/register", function(req, res) {
-    var userData = {
-      userName: req.body.userName,
-      email: req.body.email,
-      password: req.body.password,
-      gender: req.body.gender
-    };
-    db.Users.findOne({
-      where: {
-        email: req.body.email
-      }
+  app.get("/signup", authControl.signup);
+
+  app.post(
+    "/signup",
+    passport.authenticate("local-signup", {
+      successRedirect: "/profile",
+      failureRedirect: "/signup"
     })
-      .then(function(user) {
-        if (!user) {
-          var hash = bcrypt.hashSync(userData.password, 10);
-          userData.password = hash;
-          db.Users.create(userData)
-            .then(function(user) {
-              var token = jwt.sign(user.dataValues, process.env.SECRET_KEY, {
-                // eslint-disable-next-line prettier/prettier
-                expiresIn: 8.64e+7
-              });
-              res.json({ token: token });
-            })
-            .catch(function(err) {
-              res.send("error: " + err);
-            });
-        } else {
-          res.json({ err: "User already exists" });
-        }
-      })
-      .catch(function(err) {
-        res.send("error: " + err);
-      });
-  });
+  );
 
   //LOGIN
-  app.post("/api/login", function(req, res) {
-    db.Users.findOne({
-      where: {
-        email: req.body.email
-      }
+  app.get("/login", authControl.signin);
+
+  app.post(
+    "/login",
+    passport.authenticate("local-signin", {
+      successRedirect: "/post",
+      failureRedirect: "/login"
     })
-      .then(function(user) {
-        if (bcrypt.compareSync(req.body.password, user.password)) {
-          var token = jwt.sign(user.dataValues, process.env.SECRET_KEY, {
-            // eslint-disable-next-line prettier/prettier
-            expiresIn: 8.64e+7
-          });
-          res.json({ token: token });
-          console.log("Successful Login");
-        } else {
-          res.send("User does not exist");
-        }
-      })
-      .catch(function(err) {
-        res.send("error: " + err);
-      });
-  });
+  );
 
-  app.get("/api/user/:id", function(req, res) {
-    db.Users.findOne({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbUser) {
-      res.json(dbUser);
-    });
-  });
+  //PROFILE
+  app.get("/profile", isLoggedIn, authControl.profile);
 
-  app.put("/api/user/:id", function(req, res) {
-    db.Users.update(
-      {
-        userName: req.body.userName
-      },
-      {
-        where: {
-          id: req.params.id
-        }
-      }
-    ).then(function(dbUser) {
-      res.json(dbUser);
-      console.log("Updated Username");
-    });
-  });
+  app.get("/logout", authControl.logout);
 
-  app.put("/api/user/:id/passwordChange", function(req, res) {
-    var hash = bcrypt.hashSync(req.body.password, 10);
-    db.Users.update(
-      {
-        password: hash
-      },
-      {
-        where: {
-          id: req.params.id
-        }
-      }
-    ).then(function(dbUser) {
-      res.json(dbUser);
-      console.log("Updated Password");
-    });
-  });
+  function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect("/login");
+  }
 
-  app.delete("/api/user/:id", function(req, res) {
-    db.Users.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbUser) {
-      res.json(dbUser);
-      console.log("Changed Password");
-    });
-  });
+  //EVERYTHING BELOW IS JUNK ATM
+  // app.put("/api/user/:id", function(req, res) {
+  //   db.Users.update(
+  //     {
+  //       userName: req.body.userName
+  //     },
+  //     {
+  //       where: {
+  //         id: req.params.id
+  //       }
+  //     }
+  //   ).then(function(dbUser) {
+  //     res.json(dbUser);
+  //     console.log("Updated Username");
+  //   });
+  // });
+
+  // app.put("/api/user/:id/passwordChange", function(req, res) {
+  //   var hash = bcrypt.hashSync(req.body.password, 10);
+  //   db.Users.update(
+  //     {
+  //       password: hash
+  //     },
+  //     {
+  //       where: {
+  //         id: req.params.id
+  //       }
+  //     }
+  //   ).then(function(dbUser) {
+  //     res.json(dbUser);
+  //     console.log("Updated Password");
+  //   });
+  // });
+
+  // app.delete("/api/user/:id", function(req, res) {
+  //   db.Users.destroy({
+  //     where: {
+  //       id: req.params.id
+  //     }
+  //   }).then(function(dbUser) {
+  //     res.json(dbUser);
+  //     console.log("Changed Password");
+  //   });
+  // });
 };
