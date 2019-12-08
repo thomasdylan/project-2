503cc1e9b66aa0237992fd4d469e9489e5309a96
Randamus <rcviets@gmail.com>
2019-12-08 14:26:35 -0700

    delete test passed

--- test/delete.test.js
+++ test/delete.test.js
@@ -2,7 +2,7 @@ var chai = require("chai");
 var chaiHttp = require("chai-http");
 var server = require("../server");
 var db = require("../models");
-var should = chai.should();
+var expect = chai.expect;
 
 //Setting up the chai http plugin
 chai.use(chaiHttp);
@@ -22,32 +22,22 @@ describe("DELETE /api/reviews/:id", function() {
   it("should delete a review", function(done) {
     //Add an example to be deleted
     db.Post.bulkCreate([
-      { title: "Review Title", rating: 4, tmi: "Review Description" },
+      { id: 1, title: "Review Title", rating: "4", tmi: "Review Description" },
     ]).then(function() {
       //Request the route that deletes a review
-      request.get("/api/reviews/:id").end(function(err, res) {
+      request.delete("/api/reviews/1").end(function(err, res) {
         let responseStatus = res.status;
         let responseBody = res.body;
 
         //Run assertions on the response
 
-        should.not.exist(err);
+        expect(err).to.be.null;
 
-        responseStatus.should.have.status(200);
+        expect(responseStatus).to.equal(200);
 
-        responseBody
-          .should.be.a("object")
-          .should.have.property("REMOVED");
+        expect(responseBody).to.not.include({ id: 1, title: "Review Title", rating: "4", tmi: "Review Description" });
 
-        responseBody.REMOVED
-          .should.be.a("object")
-          .should.have.property("title")
-          .should.have.property("rating")
-          .should.have.property("tmi")
-          .should.have.property("id")
-          .title.should.equal("Review Title")
-          .rating.should.equal(4)
-          .tmi.should.equal("Review Description");
+        done();
       });
     });
   });
